8057477e4e430753b4036c946f511c29ecd3cac8
surya5826 <111082050+surya5826@users.noreply.github.com>
2023-02-08 14:23:05 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-   CLAIMS_URL:'http://20.163.181.235:9000',
-   FACILITY_URL:'http://20.163.181.235:9001',
-   CUSTOMER_URL:'http://20.163.181.235:9002'
+   CLAIMS_URL:'http://34.225.215.73:9000',
+   FACILITY_URL:'http://34.225.215.73:9001',
+   CUSTOMER_URL:'http://34.225.215.73:9002'
 };
 
 /*
